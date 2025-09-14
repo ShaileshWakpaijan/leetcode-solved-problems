36a2b6ad88fc6cbb297d1a7f54fecb6a864ef9e2
Shailesh <shaileshwakpaijan@gmail.com>
2025-09-14 23:36:49 +0530

    remove element using two pointer solution

--- 3. Remove Element.py
+++ 3. Remove Element.py
@@ -1,6 +1,17 @@
+# class Solution:
+#     def removeElement(self, nums, val):
+#         for i in range(len(nums)-1, -1, -1):
+#             if nums[i] == val:
+#                 nums.pop(i)
+#         return len(nums)
+
+
 class Solution:
     def removeElement(self, nums, val):
-        for i in range(len(nums)-1, -1, -1):
-            if nums[i] == val:
-                nums.pop(i)
-        return len(nums)
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
+        return i
+    
