8b390f7e7c3cf86bf7288e86fc60a40e3872c73b
Shailesh <shaileshwakpaijan@gmail.com>
2025-09-14 20:14:04 +0530

    TwoSum using two pass hash table solution

--- 1. TwoSum.py
+++ 1. TwoSum.py
@@ -1,7 +1,24 @@
+# class Solution:
+#     def twoSum(self, nums, target):
+#         for i in range(len(nums) - 1):
+#             for j in range(i+1, len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
+#         return[]
+
+
+# Two-pass Hash Table
 class Solution:
     def twoSum(self, nums, target):
-        for i in range(len(nums) - 1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return[]
+        numMap = {}
+        
+        for i in range(len(nums)):
+            numMap[nums[i]] = i
+        print(numMap)
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in numMap and numMap[complement] != i:
+                return [i, numMap[complement]]
+        else:
+            return []
