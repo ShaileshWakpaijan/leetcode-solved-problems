6cc1fba0b08ef15e81832705862872d33cbded41
Shailesh <shaileshwakpaijan@gmail.com>
2025-09-14 20:31:28 +0530

    remove duplicate using 2 pointer solution

--- 2. Remove Duplicate From Sorted Array.py
+++ 2. Remove Duplicate From Sorted Array.py
@@ -1,11 +1,22 @@
+# class Solution:
+#     def removeDuplicates(self, nums):
+#         res = []
+#         temp = None
+#         for i in nums:
+#             if temp != i:
+#                 temp = i
+#                 res.append(i)
+#         nums.clear()
+#         nums.extend(res)
+#         return len(nums)
+
+# Using 2 pointer
 class Solution:
     def removeDuplicates(self, nums):
-        res = []
-        temp = None
-        for i in nums:
-            if temp != i:
-                temp = i
-                res.append(i)
-        nums.clear()
-        nums.extend(res)
-        return len(nums)
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[i] != nums[j]:
+                i += 1
+                nums[i] = nums[j]
+
+        return i + 1    
