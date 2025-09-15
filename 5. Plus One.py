aa4074b81549bc4ad6eb497f65402be4c4cf995f
Shailesh <shaileshwakpaijan@gmail.com>
2025-09-15 20:39:23 +0530

    plus one optimal solution

--- 5. Plus One.py
+++ 5. Plus One.py
@@ -1,3 +1,16 @@
+# class Solution:
+#     def plusOne(self, digits):
+#         return list(map(int, str(int("".join(map(str, digits))) + 1)))
+
+
 class Solution:
-    def plusOne(self, digits):
-        return list(map(int, str(int("".join(map(str, digits))) + 1)))
+    def plusOne(self, digits):  
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+                if i == 0:
+                    return [1] + digits
+            else:
+                digits[i] += 1
+                break
+        return digits
