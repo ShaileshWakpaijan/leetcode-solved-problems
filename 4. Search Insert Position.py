2bd21518ef3a80311b99b473ffd281787f21d6db
Shailesh <shaileshwakpaijan@gmail.com>
2025-09-15 19:47:22 +0530

    search insert using binary search

--- 4. Search Insert Position.py
+++ 4. Search Insert Position.py
@@ -1,8 +1,22 @@
+# class Solution:
+#     def searchInsert(self, nums, target):
+#         count = 0
+#         while count < len(nums) and nums[count] < target:
+#             count += 1
+#         return count
+
+
 class Solution:
     def searchInsert(self, nums, target):
-        count = 0
-        while count < len(nums) and nums[count] < target:
-            count += 1
-        return count
-
-print(Solution().searchInsert([1,3,5,6],1))
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            print(low, mid, high)
+            if target == nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return low
