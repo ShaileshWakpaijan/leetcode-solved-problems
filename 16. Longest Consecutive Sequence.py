0f42645b83d6271f10cafaedbed5d029d1860fe9
Shailesh <shaileshwakpaijan@gmail.com>
2025-09-19 15:29:41 +0530

    longest consecutive sequence solution

---
+++ 16. Longest Consecutive Sequence.py
@@ -0,0 +1,30 @@
+class Solution:
+    def longestConsecutive(self, nums):
+        if len(nums) < 1:
+            return 0
+        sortedNums = sorted(nums)
+        maxCount = 0
+        temp = 0
+        for i in range(1, len(sortedNums)):
+            if sortedNums[i] - sortedNums[i-1] == 1:
+                temp += 1
+            elif sortedNums[i] == sortedNums[i-1]:
+                continue
+            else:
+                maxCount = temp if temp > maxCount else maxCount
+                temp = 0
+        else:
+            maxCount = temp if temp > maxCount else maxCount
+            temp = 0
+
+        return maxCount + 1
+            
+        
+
+nums = [100,4,200,1,201,202, 203, 204,3,2]
+nums2 = [0,3,7,2,5,8,4,6,0,1]
+nums3 = [1,0,1,2]
+
+print(Solution().longestConsecutive(nums))
+print(Solution().longestConsecutive(nums2))
+print(Solution().longestConsecutive(nums3))
