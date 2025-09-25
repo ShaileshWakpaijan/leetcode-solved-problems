3153a4919060f3eb88ab204cd5eb12eac12355d9
Shailesh <shaileshwakpaijan@gmail.com>
2025-09-25 21:13:54 +0530

    container with most water using 2 pointer

--- 24. Container With Most Water.py
+++ 24. Container With Most Water.py
@@ -1,10 +1,24 @@
 # brute force approach
+# class Solution:
+#     def maxArea(self, height):
+#         maxAr = 0
+#         for i in range(len(height)):
+#             for j in range(i + 1, len(height)):
+#                 maxAr = max(min(height[i], height[j]) * (j - i), maxAr)
+        
+#         return maxAr
+
+####### Using 2 Pointer #######
 class Solution:
     def maxArea(self, height):
         maxAr = 0
-        for i in range(len(height)):
-            for j in range(i + 1, len(height)):
-                maxAr = max(min(height[i], height[j]) * (j - i), maxAr)
+        l, r = 0, len(height) - 1
+        while l < r:
+            maxAr = max(min(height[l], height[r]) * (r - l), maxAr)
+            if height[l] <= height[r]:
+                l += 1
+            else:
+                r -= 1
         
         return maxAr
 
