7668e94e3aab4e7383d01c81eff73a984f378a32
Shailesh <shaileshwakpaijan@gmail.com>
2025-10-06 22:00:37 +0530

    reverse string solution

---
+++ 37. Reverse String.py
@@ -0,0 +1,14 @@
+# Leetcode 344
+class Solution:
+    def reverseString(self, s):
+        l, r = 0, len(s) - 1
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
+        
+
+        
+a = ["h","e","l","l","o"]
+print(Solution().reverseString(a))
+print(a)
