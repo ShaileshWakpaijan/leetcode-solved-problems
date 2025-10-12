c28990639c67a7ea78f315d1e16ae43559d7c7d3
Shailesh <shaileshwakpaijan@gmail.com>
2025-10-12 20:44:42 +0530

    reverse linked list solution

---
+++ 38. Reverse Linked List.py
@@ -0,0 +1,12 @@
+# Leetcode 206
+class Solution:
+    def reverseList(self, head):
+        prev, crr = None, head
+
+        while crr:
+            print(crr)
+            temp = crr.next
+            crr.next = prev
+            prev = crr
+            crr = temp
+        return prev
