98377df316da76233aa5308fa0e37558df46e12a
Shailesh <shaileshwakpaijan@gmail.com>
2025-10-16 11:49:23 +0530

    linked list cycle using floyd's algorithm

--- 41. Linked List Cycle.py
+++ 41. Linked List Cycle.py
@@ -2,11 +2,21 @@
 
 class Solution:
     def hasCycle(self, head):
-        occurs = set()
-        crr = head
-        while crr:
-            if crr in occurs:
+    #     occurs = set()
+    #     crr = head
+    #     while crr:
+    #         if crr in occurs:
+    #             return True
+    #         occurs.add(crr)
+    #         crr = crr.next
+    #     return False
+
+        ##### Using Floyd's Tortoise and Hare Algorithm #####
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            
+            if slow == fast:
                 return True
-            occurs.add(crr)
-            crr = crr.next
         return False
