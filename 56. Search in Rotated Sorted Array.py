0589284fbb2d8d17da1b76d2b14c17dd98e96bb8
Shailesh <shaileshwakpaijan@gmail.com>
2025-11-01 20:39:22 +0530

    search in roated sorted array using findmin and binary search on both parts

--- 56. Search in Rotated Sorted Array.py
+++ 56. Search in Rotated Sorted Array.py
@@ -1,26 +1,58 @@
 # LeetCode 33
 
+# class Solution:
+#     def search(self, nums, target):
+#         l, r = 0, len(nums) - 1
+
+#         while l <= r:
+#             mid = l + ((r - l) // 2)
+#             print(nums[l], nums[mid], nums[r])
+#             if nums[mid] == target:
+#                 return mid
+
+#             # Left Half
+#             if nums[l] <= nums[mid]:
+#                 if target > nums[mid] or target < nums[l]:
+#                     l = mid + 1
+#                 else:
+#                     r = mid - 1
+            
+#             # Right Half
+#             else:
+#                 if target < nums[mid] or target > nums[r]:
+#                     r = mid - 1
+#                 else:
+#                     l = mid + 1
+#         return -1
+    
+
+# Using the findMin to split and then do binary search on both sides
 class Solution:
     def search(self, nums, target):
         l, r = 0, len(nums) - 1
+        
+        while l < r:
+            mid = l + ((r - l) // 2)
+            if nums[mid] < nums[r]:
+                r = mid
+            else:
+                l = mid + 1
+        
+        res = self.binarySearch(0, l - 1, nums, target)
+        if res != -1:
+            return res
+        return self.binarySearch(l, len(nums) - 1, nums, target)
+        
+    def binarySearch(self, left, right, nums, target):
+        l, r = left, right
 
         while l <= r:
             mid = l + ((r - l) // 2)
-            print(nums[l], nums[mid], nums[r])
-            if nums[mid] == target:
-                return mid
 
-            # Left Half
-            if nums[l] <= nums[mid]:
-                if target > nums[mid] or target < nums[l]:
-                    l = mid + 1
-                else:
-                    r = mid - 1
-            
-            # Right Half
+            if target < nums[mid]:
+                r = mid - 1
+            elif target > nums[mid]:
+                l = mid + 1
             else:
-                if target < nums[mid] or target > nums[r]:
-                    r = mid - 1
-                else:
-                    l = mid + 1
+                return mid
         return -1
